560cae1fa539fa0975379fb9d72a6df7180b7d84
saketh1106 <sakethgumpalli@gmail.com>
2026-02-16 13:18:02 +0530

    auto create admin

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@
 
 pip install -r requirements.txt
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" \
+| python manage.py shell
