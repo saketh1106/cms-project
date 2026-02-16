729a5f84c569582741f909b596a0852f93b8e189
saketh1106 <sakethgumpalli@gmail.com>
2026-02-16 13:29:04 +0530

    fix superuser creation

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 pip install -r requirements.txt
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-User.objects.filter(username='admin').exists() or \
-User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" \
-| python manage.py shell
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+END
